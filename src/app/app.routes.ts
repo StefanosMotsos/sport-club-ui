6f62f0145c6fb0968143d9e8dc71ab7f6da29a9e
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-06-11 23:41:11 +0300

    feat: added footer and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {Layout} from './core/layout/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Layout,
+    children: [
+
+    ]
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Footer} from './core/layout/footer/footer';
-import {Header} from './core/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
